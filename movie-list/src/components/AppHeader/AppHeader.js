8f5a021c6d42794334832effd71911d1d97a1184
nikolaspasojevic95 <nikola.spasojevic@factoryww.com>
2021-07-21 10:58:38 +0200

    sorting/all-components-sorted-into-unique-folders

---
+++ movie-list/src/components/AppHeader/AppHeader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Menu, Dropdown, Button } from "antd";
+import "./AppHeader.css";
+
+function AppHeader() {
+  const menu = (
+    <Menu>
+      <Menu.Item key="1">MOVIE</Menu.Item>
+      <Menu.Item key="2">SERIES</Menu.Item>
+      <Menu.Item key="3">TV SHOW</Menu.Item>
+    </Menu>
+  );
+  return (
+    <>
+      <Dropdown overlay={menu} className="btn-dropdown">
+        <Button>TYPE</Button>
+      </Dropdown>
+    </>
+  );
+}
+
+export default AppHeader;
